76cc8e72dd3d1ea13116ead9b51a465420127c95
Raphael R <reis.rrsc@outlook.com>
2026-01-12 10:53:13 -0300

    ajuste app para atender controller

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,17 @@
 import * as http from 'http';
 import pool from './db';
-
-const getBody = (req: http.IncomingMessage): Promise<any> => {
-    return new Promise((resolve, reject) => {
-        let body = '';
-        
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-
-        req.on('end', () => {
-            try {
-                resolve(body ? JSON.parse(body) : {});
-            } catch (error) {
-                reject(error);
-            }
-        });
-    });
-};
+import { HttpMethod } from './utils/http-methods';
+import { Routes } from './router/routes';
+import { cadastrarConta, getTodasContas } from './controllers/contas/contas-controller';
 
 export const app = async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    
+    const baseUrl = req.url?.split("?")[0];
+
+    if (req.method === HttpMethod.GET && baseUrl === Routes.LISTA_CONTAS) {
+        await getTodasContas(req, res);
+    }
+
+    if (req.method === HttpMethod.POST && baseUrl === Routes.CADASTRAR_CONTA){
+        await cadastrarConta(req, res);
+    }
 }
