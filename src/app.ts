8804963f47be54c84dc4c609799d48403eb5b78b
Raphael R <reis.rrsc@outlook.com>
2026-01-12 09:09:58 -0300

    conexão com banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,23 @@
 import * as http from 'http';
+import pool from './db';
+
+const getBody = (req: http.IncomingMessage): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        let body = '';
+        
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            try {
+                resolve(body ? JSON.parse(body) : {});
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+};
 
 export const app = async (req: http.IncomingMessage, res: http.ServerResponse) => {
     
