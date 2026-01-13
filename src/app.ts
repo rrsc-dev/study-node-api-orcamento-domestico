2f3921244fe13213e464c720cb36a210bb0b5e75
Raphael R <reis.rrsc@outlook.com>
2026-01-13 08:11:14 -0300

    add rota para o getContaById

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import * as http from 'http';
 import pool from './db';
 import { HttpMethod } from './utils/http-methods';
 import { Routes } from './router/routes';
-import { cadastrarConta, getTodasContas } from './controllers/contas/contas-controller';
+import { cadastrarConta, getContaById, getTodasContas } from './controllers/contas/contas-controller';
 
 export const app = async (req: http.IncomingMessage, res: http.ServerResponse) => {
     const baseUrl = req.url?.split("?")[0];
 
     if (req.method === HttpMethod.GET && baseUrl === Routes.LISTA_CONTAS) {
         await getTodasContas(req, res);
+    }
+
+    if (req.method === HttpMethod.GET && baseUrl === Routes.GET_CONTA_BY_ID) {
+        await getContaById(req, res);
     }
 
     if (req.method === HttpMethod.POST && baseUrl === Routes.CADASTRAR_CONTA){
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,4 +1,5 @@
 export enum Routes {
     LISTA_CONTAS = '/api/contas/getTodasContas',
     CADASTRAR_CONTA = '/api/contas/cadastrarConta',
+    GET_CONTA_BY_ID = '/api/contas/getContaById'
 }
