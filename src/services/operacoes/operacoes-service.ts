1da2f499f0f90000efad93fc1420b3e65bc9ae78
Raphael R <reis.rrsc@outlook.com>
2026-01-16 08:54:19 -0300

    add getOperacaoByIdService

--- src/services/operacoes/operacoes-service.ts
+++ src/services/operacoes/operacoes-service.ts
@@ -1,5 +1,5 @@
 import { OperacaoModel } from "../../models/operacao-model";
-import { cadastrarOperacao, getTodasOperacoes } from "../../repositories/operacoes/operacoes-repository";
+import { cadastrarOperacao, getOperacaoById, getTodasOperacoes } from "../../repositories/operacoes/operacoes-repository";
 
 export const cadastrarOperacaoService = async (operacao: OperacaoModel): Promise<OperacaoModel> => {
     if (!operacao.tipo && !operacao.valor) {
@@ -12,6 +12,30 @@ export const cadastrarOperacaoService = async (operacao: OperacaoModel): Promise
 
 export const getTodasOperacoesService = async (): Promise<OperacaoModel[]> => {
     const data = await getTodasOperacoes();
+
+    return data;
+}
+
+export const getOperacaoByIdService = async (operacaoId: string | undefined): Promise<OperacaoModel> => {
+    const match = operacaoId?.match(/[?&]id=(\d+)/);
+
+    const idParam = match ? match[1] : null;
+
+    if (!idParam) {
+        throw new Error("ID não fornecido");
+    }
+
+    const id = parseInt(idParam,10);
+
+    if (isNaN(id)) {
+        throw new Error("ID inválido");
+    }
+
+    const data = await getOperacaoById(id);
+
+    if (!data) {
+        throw new Error("Operação não encontrada");
+    }
 
     return data;
 }
