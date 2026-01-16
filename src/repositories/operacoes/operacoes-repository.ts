83f13494c76e6bf5fefbb9ef5a71e290d9560abc
Raphael R <reis.rrsc@outlook.com>
2026-01-16 08:48:13 -0300

    funções de operação repo

--- src/repositories/operacoes/operacoes-repository.ts
+++ src/repositories/operacoes/operacoes-repository.ts
@@ -19,13 +19,20 @@ export const getOperacaoById = async (id: number): Promise<OperacaoModel> => {
     return rows[0];
 }
 
-// export const excluirOperacao = async (id: number): Promise<void> => {
-//     // TODO
-// }
+export const excluirOperacao = async (id: number): Promise<void> => {
+    const query = `DELETE FROM operacoes WHERE id = $1`;
+    await pool.query(query, [id]);
+}
 
-// export const alterarStatusOperacao = async (id: number, status: number): Promise<OperacaoModel> => {
-//     // TODO
-// }
+export const alterarStatusOperacao = async (id: number, status: number): Promise<OperacaoModel> => {
+    const query = `UPDATE operacoes SET status = $1 WHERE id = $2 RETURNING *`;
+
+    const values = [status, id];
+
+    const { rows } = await pool.query<OperacaoModel>(query, values);
+
+    return rows[0];
+}
 
 export const cadastrarOperacao = async (operacao: OperacaoModel): Promise<OperacaoModel> => {
     const query = `INSERT INTO operacoes (tipo, data, valor, conta_id, status) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
@@ -35,9 +42,14 @@ export const cadastrarOperacao = async (operacao: OperacaoModel): Promise<Operac
     const { rows } = await pool.query<OperacaoModel>(query, values);
 
     return rows[0];
-    
 }
 
-// export const editarOperacao = async (id: number, chave: string, novoValor: string): Promise<OperacaoModel> => {
-//     // TODO
-// }
+export const editarOperacao = async (id: number, chave: string, novoValor: string): Promise<OperacaoModel> => {
+    const query = `UPDATE operacoes SET $1 = $2 WHERE id = $3`;
+
+    const values = [chave, novoValor, id];
+
+    const { rows } = await pool.query<OperacaoModel>(query, values);
+
+    return rows[0];
+}
