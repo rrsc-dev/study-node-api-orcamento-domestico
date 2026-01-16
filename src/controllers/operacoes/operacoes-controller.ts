ad72caa0c83215e325025706ab62c16a7fa008e9
Raphael R <reis.rrsc@outlook.com>
2026-01-16 08:33:55 -0300

    get todas operações controller

--- src/controllers/operacoes/operacoes-controller.ts
+++ src/controllers/operacoes/operacoes-controller.ts
@@ -1,7 +1,19 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { OperacaoModel } from "../../models/operacao-model";
 import { getBody } from "../../utils/body-parser";
-import { cadastrarOperacaoService } from "../../services/operacoes/operacoes-service";
+import { cadastrarOperacaoService, getTodasOperacoesService } from "../../services/operacoes/operacoes-service";
+
+export const getTodasOperacoes = async (req: IncomingMessage, res: ServerResponse) => {
+    try {
+        const operacoes = await getTodasOperacoesService();
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(operacoes));
+    } catch (error) {
+        res.writeHead(500);
+        res.end(JSON.stringify({ message: 'Erro ao buscar contas' }));
+    }
+}
 
 export const cadastrarOperacao = async (req: IncomingMessage, res: ServerResponse) => {
     try {
