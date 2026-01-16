628611f255d66f0e4d36919c1e585b3e4150519a
Raphael R <reis.rrsc@outlook.com>
2026-01-16 08:09:53 -0300

    add cadastro de operação controller

---
+++ src/controllers/operacoes/operacoes-controller.ts
@@ -0,0 +1,21 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { OperacaoModel } from "../../models/operacao-model";
+import { getBody } from "../../utils/body-parser";
+import { cadastrarOperacaoService } from "../../services/operacoes/operacoes-service";
+
+export const cadastrarOperacao = async (req: IncomingMessage, res: ServerResponse) => {
+    try {
+        const body = await getBody(req) as OperacaoModel
+
+        const novaOperacao = await cadastrarOperacaoService(body);
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(novaOperacao));
+    } catch (error) {
+
+        console.log('ERRO: ', error);
+
+        res.writeHead(500);
+        res.end(JSON.stringify({ message: 'Erro ao cadastrar operação' }));
+    }
+}
