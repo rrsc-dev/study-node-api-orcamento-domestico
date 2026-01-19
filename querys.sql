a227761bc2a688a3763bb8a2f118256a57bc56c2
Raphael R <reis.rrsc@outlook.com>
2026-01-19 15:52:44 -0300

    categorias e subcategorias na query

--- querys.sql
+++ querys.sql
@@ -24,3 +24,45 @@ CREATE TABLE operacoes (
         ON UPDATE CASCADE
         ON DELETE RESTRICT
 );
+
+CREATE TABLE categorias (
+    id SERIAL PRIMARY KEY,
+    nome_categoria VARCHAR(100) NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT TRUE,
+    uso INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE subcategorias (
+    id SERIAL PRIMARY KEY,
+    categoria_id INT NOT NULL,
+    nome_subcategoria VARCHAR(100) NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_subcategorias_categorias
+        FOREIGN KEY (categoria_id)
+        REFERENCES categorias (id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+);
+
+ALTER TABLE operacoes
+ADD COLUMN categoria_id INT,
+ADD COLUMN subcategoria_id INT;
+
+ALTER TABLE operacoes
+ADD CONSTRAINT fk_operacoes_categoria
+    FOREIGN KEY (categoria_id)
+    REFERENCES categorias (id)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL;
+
+ALTER TABLE operacoes
+ADD CONSTRAINT fk_operacoes_subcategoria
+    FOREIGN KEY (subcategoria_id)
+    REFERENCES subcategorias (id)
+    ON UPDATE CASCADE
+    ON DELETE SET NULL;
