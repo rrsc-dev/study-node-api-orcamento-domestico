032a108e50a9371bf0610d502f549066331f2441
Raphael R <reis.rrsc@outlook.com>
2026-01-19 15:53:25 -0300

    add subcategoria model

---
+++ src/models/subcategoria-model.ts
@@ -0,0 +1,8 @@
+export interface SubcategoriaModel {
+    id: number;
+    categoriaId: number;
+    status: number;
+    nomeSubcategoria: string;
+    created_at?: string;
+    updated_at?: string;
+}
